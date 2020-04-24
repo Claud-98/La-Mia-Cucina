e168ec424c6471d1b06591e977878531f183f6b4
Claudio <claudio.1998P@gmail.com>
2020-04-24 16:53:46 +0200

    Secondo commit: MainActivity TopAppBar
    Aggiunta La TopAppBar della main activity

--- app/src/main/java/com/example/lamiacucina/MainActivity.java
+++ app/src/main/java/com/example/lamiacucina/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.lamiacucina;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
 
 import android.os.Bundle;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        androidx.appcompat.widget.Toolbar toolbar;
+        toolbar = findViewById(R.id.main_activity_toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setNavigationIcon(getResources().getDrawable(R.drawable.icon_menu));
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+    getMenuInflater().inflate(R.menu.menu,menu);
+        return true;
     }
 }
